f7025ac841fe5ddee77a7e5d9cbcf10698a02c64
Christian Geer <cgeer98@gmail.com>
2023-05-17 14:18:41 -0400

    combining season stats and tourn wins

--- data_cleaning/stats_wins.py
+++ data_cleaning/stats_wins.py
@@ -2,14 +2,8 @@
 import pandas as pd
 
 # load season stats and tournament wins data
-stats = pd.read_csv('cleaned_data/TeamSeasonAverages.csv')
-wins = pd.read_csv('cleaned_data/tournament_wins.csv')
+season_stats = pd.read_csv('cleaned_data/TeamSeasonAverages.csv')
+tourn_wins = pd.read_csv('cleaned_data/tournament_wins.csv')
 
 # join the two data tables
-combined = stats.merge(wins, how='right', left_on=['TeamID','Season'], right_on=['WTeamID', 'Season'])
-
-count_teams = combined.groupby(by='Season').count()
-print(count_teams)
-
-# print
-# print(combined)
+combined = season_stats.merge(tourn_wins, how='right', on=['TeamID','Season'])
