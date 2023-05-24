412c0700283bef84ffea0a2dac41573ff0bf24c5
Christian Geer <cgeer98@gmail.com>
2023-05-24 10:21:54 -0400

    removing extra col

--- data_cleaning/stats_wins.py
+++ data_cleaning/stats_wins.py
@@ -8,5 +8,8 @@ tourn_wins = pd.read_csv('cleaned_data/tournament_wins.csv')
 # join the two data tables
 combined = season_stats.merge(tourn_wins, how='right', on=['TeamID','Season'])
 
+# remove extra index col from merge
+combined = combined.drop(columns=['Unnamed: 0'])
+
 # write to csv
 combined.to_csv('cleaned_data/final_data.csv')
